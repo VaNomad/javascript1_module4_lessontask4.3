671276aec93cb1fae09bf0c2cc85a68ac01ced91
VaNomad <sjur.hassel@gmail.com>
2022-04-27 14:00:30 +0100

    Set up ready

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// ——————‡——————  Question 1  ——————‡——————
+/* 
+When a user selects a number from the select box, build a number of anchor tags (a elements) that matches the selected number
+the anchor tags should link to the details page with the id in the query string
+on the details page retrieve the id from the query string
+add the id to the URL below, make an API call using the final url and display the name and occupation of the selected character
+*/
