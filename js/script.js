796be54604664d01b085a6b5e9948dbceac346b1
VaNomad <sjur.hassel@gmail.com>
2022-04-27 19:00:12 +0100

    starting

--- js/script.js
+++ js/script.js
@@ -5,3 +5,14 @@ the anchor tags should link to the details page with the id in the query string
 on the details page retrieve the id from the query string
 add the id to the URL below, make an API call using the final url and display the name and occupation of the selected character
 */
+
+const select = document.querySelector("select");
+
+const listContainer = document.querySelector(".list");
+
+function newList(event) {
+  console.log("newList:", arguments);
+  
+}
+listContainer.addEventListener("click", newList);
+
