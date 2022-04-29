0e493102b4c95a66a542b2128e547064fbfb56be
VaNomad <sjur.hassel@gmail.com>
2022-04-29 14:32:34 +0100

    copy pasting code

--- js/script.js
+++ js/script.js
@@ -24,3 +24,15 @@ function newList(event) {
   }
 }
 
+const form = document.querySelector("#contactForm");
+
+form.onsubmit = function () {
+    event.preventDefault();
+
+    console.log(event);
+
+    const name = document.querySelector("#name");
+
+    console.log(name.value);
+};
+
